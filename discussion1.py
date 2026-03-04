ee77432b6bb147c83121dba564f676850f361570
Fuaad <fuaadmohamoud@gmail.com>
2026-03-04 08:19:57 -0600

    completed isprime

--- discussion1.py
+++ discussion1.py
@@ -69,5 +69,37 @@ def fizzbuzz(n):
         i+=1
 
 
+
+def is_prime(n):
+    """
+    >>> is_prime(10)
+    False
+    >>> is_prime(7)
+    True
+    >>> is_prime(1) # one is not a prime number!!
+    False
+    """
+    "*** YOUR CODE HERE ***"
+
+    i = 2
+    
+    if n < 2:
+        return False
+
+    if n == 2:
+        return True
+
+    while i < n:
+
+        if n % i == 0:
+            return False
+        i+=1
+    
+    return True
+
+
+
 if __name__ == "__main__":
-    fizzbuzz(16)
+    print(is_prime(10))
+    print(is_prime(7))
+    print(is_prime(1) )
