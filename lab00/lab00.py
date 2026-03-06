9b494c492671d41ca0efff38e3555995bf760dbb
Fuaad <fuaadmohamoud@gmail.com>
2026-03-06 09:02:55 -0600

    completed divisible_by_k

--- lab00/lab00.py
+++ lab00/lab00.py
@@ -16,6 +16,39 @@ def twenty_twenty_four():
     return g(1000) + g(g(f(f(f(f(g(5)))))))
 
 
-if __name__ == "__main__":
-    print( twenty_twenty_four())
+
+def divisible_by_k(n, k):
+    """
+    >>> a = divisible_by_k(10, 2)  # 2, 4, 6, 8, and 10 are divisible by 2
+    2
+    4
+    6
+    8
+    10
+    >>> a
+    5
+    >>> b = divisible_by_k(3, 1)  # 1, 2, and 3 are divisible by 1
+    1
+    2
+    3
+    >>> b
+    3
+    >>> c = divisible_by_k(6, 7)  # There are no integers up to 6 divisible by 7
+    >>> c
+    0
+    """
+    "*** YOUR CODE HERE ***"
+
+    i = 1
+    res = 0
+    while i <=n:
+        if i % k == 0:
+            print(i)
+            res+=1
+        i+=1
+    return res
     
+
+
+if __name__ == "__main__":
+    print(divisible_by_k(10, 2))
