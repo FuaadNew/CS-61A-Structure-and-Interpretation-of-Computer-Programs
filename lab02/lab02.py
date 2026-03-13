6772cc6dd3d0ad0d8a62003aa1c5e0b3ccdd5e23
Fuaad <fuaadmohamoud@gmail.com>
2026-03-13 08:40:01 -0500

    added match_k from discussion 2

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -164,6 +164,32 @@ def find_digit(k):
     return lambda digit: digit // 10 ** (k-1) % 10
     
 
+def match_k(k):
+    """Returns a function that checks if digits k apart match.
+
+    >>> match_k(2)(1010)
+    True
+    >>> match_k(2)(2010)
+    False
+    >>> match_k(1)(1010)
+    False
+    >>> match_k(1)(1)
+    True
+    >>> match_k(1)(2111111111111111)
+    False
+    >>> match_k(3)(123123)
+    True
+    >>> match_k(2)(123123)
+    False
+    """
+    def check(x):
+        while x // (10 ** k) > 0:
+            if ____________________________:
+                return ____________________________
+            x //= 10
+        ____________________________
+    ____________________________
+
 
 
 
