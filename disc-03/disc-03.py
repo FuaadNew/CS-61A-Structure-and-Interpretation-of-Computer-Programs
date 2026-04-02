d5ba94566aa051dba8268f8dec4c6799827f2988
Fuaad <fuaadmohamoud@gmail.com>
2026-04-02 10:01:52 -0500

    started recursive is_prime

--- disc-03/disc-03.py
+++ disc-03/disc-03.py
@@ -33,6 +33,19 @@ def skip_factorial(n):
 
 
 
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+
+
 if __name__ == '__main__':
     print(skip_factorial(5))
     print(skip_factorial(8))
