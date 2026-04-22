947c1c379159b3b34b143bb2e85296eea06a8c20
Fuaad <fuaadmohamoud@gmail.com>
2026-04-22 10:58:16 -0500

    Compelted cascade

--- hog/hog.py
+++ hog/hog.py
@@ -407,14 +407,18 @@ if __name__ == '__main__':
         
 
         def cascade(n):
-            if n <= 10:
+            if n >= 10:
+                print(n)
+            
+            else:
                 print(n)
                 return
             
-            print(n)
+           
             cascade(n//10)
             print(n)
             
+            
         
-        cascade(1234)
+        cascade(1234567890)
 
