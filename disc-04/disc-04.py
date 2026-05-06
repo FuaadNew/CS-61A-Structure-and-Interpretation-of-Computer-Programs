ff2cf230d6d5261a37e8961aabd45ba6af54cd1b
Fuaad <fuaadmohamoud@gmail.com>
2026-05-06 09:47:33 -0500

    started sums

--- disc-04/disc-04.py
+++ disc-04/disc-04.py
@@ -49,12 +49,34 @@ def max_product(s):
 
 
 
+def sums(n, m):
+    """Return lists that sum to n containing positive numbers up to m that
+    have no adjacent repeats.
 
+    >>> sums(5, 1)
+    []
+    >>> sums(5, 2)
+    [[2, 1, 2]]
+    >>> sums(5, 3)
+    [[1, 3, 1], [2, 1, 2], [2, 3], [3, 2]]
+    >>> sums(5, 5)
+    [[1, 3, 1], [1, 4], [2, 1, 2], [2, 3], [3, 2], [4, 1], [5]]
+    >>> sums(6, 3)
+    [[1, 2, 1, 2], [1, 2, 3], [1, 3, 2], [2, 1, 2, 1], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    """
+    if n < 0:
+        return []
+    if n == 0:
+        sums_to_zero = []     # The only way to sum to zero using positives
+        return [sums_to_zero] # Return a list of all the ways to sum to zero
+    result = []
+    for k in range(1, m + 1):
+        result = result + [ ___ for rest in ___ if rest == [] or ___ ]
 
 
 
 if __name__ == "__main__":
-    print(max_product([10, 3, 1, 9, 2]))
+    
    
 
    
