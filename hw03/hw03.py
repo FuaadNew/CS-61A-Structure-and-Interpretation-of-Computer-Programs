c8aac5a6535ff6ebab2be0a4a03cba5777d85cf0
Fuaad <fuaadmohamoud@gmail.com>
2026-05-12 09:36:10 -0500

    first draft of interleaved sum (bugs)

--- hw03/hw03.py
+++ hw03/hw03.py
@@ -82,9 +82,14 @@ def interleaved_sum(n, odd_func, even_func):
     """
     "*** YOUR CODE HERE ***"
     
+    if n < 0:
+        return 0
     if n == 1:
-        return odd_func()
-
+        return odd_func(n)
+    if n == 0:
+        return even_func(n)
+    
+    return interleaved_sum(n - 1, odd_func, even_func) + interleaved_sum(n - 2, odd_func, even_func)
 def next_smaller_dollar(bill):
     """Returns the next smaller bill in order."""
     if bill == 100:
@@ -210,3 +215,21 @@ def make_anonymous_factorial():
 
 if __name__ == "__main__":
    
+
+    identity = lambda x: x
+    square = lambda x: x * x
+    triple = lambda x: x * 3
+    print(interleaved_sum(5, identity, square)) # 1   + 2*2 + 3   + 4*4 + 5
+    29
+    print(interleaved_sum(5, square, identity)) # 1*1 + 2   + 3*3 + 4   + 5*5
+    41
+    print(interleaved_sum(4, triple, square))   # 1*3 + 2*2 + 3*3 + 4*4
+    32
+    print(interleaved_sum(4, square, triple))  # 1*1 + 2*3 + 3*3 + 4*3
+    28
+    #>>> from construct_check import check
+    #>>> check(HW_SOURCE_FILE, 'interleaved_sum', ['While', 'For', 'Mod']) # ban loops and %
+    #True
+    #>>> check(HW_SOURCE_FILE, 'interleaved_sum', ['BitAnd', 'BitOr', 'BitXor']) # ban bitwise operators, don't worry about these if you don't know what they are
+    #True
+
