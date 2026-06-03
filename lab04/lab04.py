e9484515ac634187c201c2887cd0ddbfd90bbd5f
Fuaad <fuaadmohamoud@gmail.com>
2026-06-03 10:29:04 -0500

    starting divide

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -7,7 +7,7 @@ def divide(quotients, divisors):
     >>> divide(range(1, 5), range(20, 25))
     {1: [20, 21, 22, 23, 24], 2: [20, 22, 24], 3: [21, 24], 4: [20, 24]}
     """
-    return {}
+    return {____: ____ for ____ in ____}
 
 
 def buy(fruits_to_buy, prices, total_amount):
