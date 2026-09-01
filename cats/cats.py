d15fc000de56867c99e5a3a4f75dfe7e2060b3a1
Fuaad <fuaadmohamoud@gmail.com>
2026-09-01 10:53:14 -0500

    fixed bug in autocorrect to update smallest word diff each iteration

--- cats/cats.py
+++ cats/cats.py
@@ -223,6 +223,7 @@ def autocorrect(typed_word, word_list, diff_function, limit):
                     smallest_word = word
             else:
                 smallest_word = word
+            smallest_diff = min(smallest_diff, word_diff)
     return smallest_word if smallest_word != "" else typed_word
 
     # END PROBLEM 5
