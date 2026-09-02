9862704fd3aa97648d12cd1f8c32a0161033e618
Fuaad <fuaadmohamoud@gmail.com>
2026-09-02 10:29:18 -0500

    simplified autocorrect to no longer keep track of indices as the linear scan takes care of tie breaking precedence

--- cats/cats.py
+++ cats/cats.py
@@ -208,22 +208,15 @@ def autocorrect(typed_word, word_list, diff_function, limit):
     'testing'
     """
     # BEGIN PROBLEM 5
-    word_list = {x:i for i,x in enumerate(word_list)} 
     if typed_word in word_list:
         return typed_word
     smallest_diff = float('inf')
     smallest_word = ""
     for word in word_list:
         word_diff = diff_function(typed_word, word, limit)
-        if word_diff <= limit and word_diff <= smallest_diff:
-            if smallest_diff == word_diff:
-                prev_closest_word = word_list[smallest_word]
-                curr_candidate =  word_list[word]
-                if curr_candidate < prev_closest_word:
-                    smallest_word = word
-            else:
-                smallest_word = word
-            smallest_diff = min(smallest_diff, word_diff)
+        if word_diff <= limit and word_diff <smallest_diff:
+            smallest_word = word
+            smallest_diff = word_diff
     return smallest_word if smallest_word != "" else typed_word
 
     # END PROBLEM 5
